69d98943f4f2a4872bb50f072c60d6bb3c9b9402
Allexandrres <alexandredasilveira13@gmail.com>
2024-08-08 10:20:04 -0300

    Add files via upload

---
+++ atividade 6/script.js
@@ -0,0 +1,7 @@
+function ligarLampada() {
+    document.getElementById('lampada').src = 'lampada.jpg';
+}
+
+function desligarLampada() {
+    document.getElementById('lampada').src = 'lampada.jpg';
+}
---
+++ atividade 7/script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    document.querySelector('.colorful-div').style.backgroundColor = '#FF0000';
+}
+
+function resetColor() {
+    document.querySelector('.colorful-div').style.backgroundColor = ' #ffcbdb';
+}
+
